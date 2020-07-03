ac76ca4838a25ae3078c613aff7993724e7304d4
thestarivore <thestarivore>
2020-07-04 01:42:46 +0200

    Implemented the first Complete Dynamic Calendar

--- nodejs-server/www/assets/js/events_calendar.js
+++ nodejs-server/www/assets/js/events_calendar.js
@@ -2,13 +2,13 @@ const bodyContainer = document.getElementById('ecalendar_table_body');
 const headContainer = document.getElementById('ecalendar_table_head');
 
 var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tue";
+weekday[3] = "Wed";
+weekday[4] = "Thu";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
 
 var monthArray = new Array(12);
 monthArray[0] = "January";
@@ -27,7 +27,10 @@ monthArray[11] = "December";
 var eventNames = [];
 var eventDates = [];
   
-fetch("v2/events")
+buildCalendar();
+
+function buildCalendar(selectedMonth){
+  fetch("v2/events")
     .then(function(response) {
         if (!response.ok) {
             throw new Error("HTTP error, status = " + response.status);
@@ -42,54 +45,177 @@ fetch("v2/events")
             eventDates[i] = new Date(Date.parse(date));
         }
 
-        today = new Date();
-        month = today.getMonth();
-        year = today.getFullYear();
-        firstDay = today.getDay();
+        //If no month selected
+        if(selectedMonth == null){
+          today = new Date();
+          month = today.getMonth();
+          year = today.getFullYear();
+          //firstDay = today.getDay();
+        } else {
+          today = new Date();
+          month = selectedMonth;
+          year = today.getFullYear();
+        }
 
         //Set the Month on the Head
         const content = `
-          <tr>
-            <th>${monthArray[month]}</th>
-          </tr>
+          <div>
+            <button id="left_month_btn" type="button" class="btn btn-default">
+              <span class="arrow left"></span>
+            </button>
+          </div>
+
+          <div>
+            <h2>${monthArray[month]}</h2>
+          </div>
+
+          <div>
+            <button id="right_month_btn" type="button" class="btn btn-default">
+              <span class="arrow right"></span>
+            </button>
+          </div>
         `;
+
         // Append newyly created card element to the container
         headContainer.innerHTML += content;
 
+        //Set OnClick Actions for the Buttons
+        //Empty the Calendar content and build it again for the selected month
+        $('#left_month_btn').on('click', function(event) {
+          headContainer.innerHTML = "";
+          bodyContainer.innerHTML = "";
+          if(month > 0)
+            buildCalendar(month-1);
+          else
+            buildCalendar(11);
+        });
+        $('#right_month_btn').on('click', function(event) {
+          headContainer.innerHTML = "";
+          bodyContainer.innerHTML = "";
+          if(month < 11)
+            buildCalendar(month+1);
+          else
+            buildCalendar(0);
+        });
+
+        //Calculate starting date (the week that contains today)
+        //var startingDate = Math.floor(today.getDate()/7) * 7 + 1;
+        var startingDay = new Date();
+        //startingDay.setDate(startingDate);
+        startingDay.setDate(1);
+        startingDay.setMonth(month);
+        startingDay.setFullYear(year);
+        firstDay = startingDay.getDay();  
+
+        numDays = daysInMonth(month, year);
+        day = firstDay;
+
         //Cycle one Week day by day
-        for(i=0; i <= 6; i++){
+        var i=1;
+        while(i <= numDays){
           var eventName = "";
-          date = today.getDate()+i;
-          day=firstDay+i;
+          //date = today.getDate()+i;
+          //date = startingDay.getDate()+i;
+          date = i;
+          //day = firstDay+i;
           if(day>6)
-            day-=6;
+            day-=7;
 
           //Find is there any event on this date
           for (var j = 0; j < eventNames.length; j++) {
-            if(eventDates[j].getDate() == date){
-              eventName = eventNames[j];
-              break;//max one event per day
+            if(isEventOnDate(j, date, month, year)){
+              if(eventName != "")
+                eventName += "<br>" + eventNames[j];
+              else
+                eventName = eventNames[j];
+              //break;//max one event per day
             }
           }
 
+          //Set the date as Bold if is the current day
+          if(date == today.getDate() && selectedMonth == null)
+            dateHTML = `<p class="font-weight-bold">${date}</p>`;
+          else
+            dateHTML = `<p>${date}</p>`;
+
           // Construct card content
           const content = `
-            <tr>
-              <td>${weekday[day]}</td>
-              <td>${date}</td>
-              <td>${eventName}</td>
+            <tr class="d-row">
+              <td class="col-md-1 bordered_column"><p>${weekday[day]}</p></td>
+              <td col-md-1>${dateHTML}</td>
+              <td col-md-10><p>${eventName}</p></td>
             </tr>
           `;
 
           // Append newyly created card element to the container
           bodyContainer.innerHTML += content;
+
+          if(isAnyEventOnDate(date, month, year) || isAnyEventOnDate(date+1, month, year) || date == numDays){
+            day++;
+            i++;
+          } else {
+            // Construct card content
+            const content = `
+              <tr class="d-row">
+                <td class="col-md-1 bordered_column discussion"></td>
+                <td class="col-md-1 discussion"></td>
+                <td class="col-md-10"></td>
+              </tr>
+            `;
+
+            // Append newyly created card element to the container
+            bodyContainer.innerHTML += content;
+
+            // Find the first date that has an Event
+            eventDate = getFirstDateWithEvent(date, month, year);
+            
+            // Get the new day of the week
+            var tmpDate = new Date();
+            tmpDate.setDate(eventDate);
+            tmpDate.setMonth(month);
+            tmpDate.setFullYear(year);
+            day = tmpDate.getDay(); 
+            
+            // Set the new day
+            i = eventDate;
+          }
         }
 
        
     });
-
+}
 
 // check how many days in a month 
 function daysInMonth(iMonth, iYear) { 
   return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
+// check id there is an event on the specified date
+function isEventOnDate(i, date, month, year) { 
+  if(eventDates[i].getDate() == date && eventDates[i].getMonth() == month && eventDates[i].getFullYear() == year)
+    return true;
+  else
+    return false;
+}
+
+// check id there is any event on the specified date
+function isAnyEventOnDate(date, month, year) { 
+  //Find is there any event on this date
+  for (var j = 0; j < eventNames.length; j++) {
+    if(isEventOnDate(j, date, month, year)){
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Starting from the specified date, find the first date that has an Event
+function getFirstDateWithEvent(date, month, year){
+  for (var j = date; j < daysInMonth(month, year); j++) {
+    if(isAnyEventOnDate(j, month, year))
+      return j; 
+  }
+
+  return daysInMonth(month, year);
 }
